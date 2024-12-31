e61e14c57fc77f51e52e91f38b75b40a90d16fc7
Ivan Tlaseca <ivantlaseca@hotmail.com>
2024-12-30 20:01:33 -0500

    Implement project card links the right way.

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -8,11 +8,9 @@ const Projects = () => {
             <h2>Projects</h2>
             <div className="projects-grid">
                 {projectData && projectData.map((project) => (
-                    <a href={project.gitHubLink}>
-                        <div className="project-card" key={project.id}>
-                            <h3>{project.title}</h3>
-                            <p>{project.description}</p>
-                        </div>
+                    <a className="project-card" key={project.id} href={project.gitHubLink}>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
                     </a>
                 ))
                 }
