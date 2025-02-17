1b19be3e84658bb981b50b6e02aa319c124aa728
Ivan Tlaseca <ivantlaseca@hotmail.com>
2025-02-17 14:32:18 -0500

    Fix formatting and next.config

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,16 +1,16 @@
 // components/Footer.jsx
 
 const Footer = () => {
-    return (
-      <>
-        <hr/>
-        <div className="footer-container">
-          <p>
-            © {new Date().getFullYear()} Ivan's Portfolio
-          </p>
-        </div>
-      </>
-    )
-  }
-  
-  export default Footer;
+  return (
+    <>
+      <hr />
+      <div className="footer-container">
+        <p>
+          © {new Date().getFullYear()} Ivan&apos;s Portfolio
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Footer;
