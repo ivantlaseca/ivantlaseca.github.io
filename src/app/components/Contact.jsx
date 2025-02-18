aecb36c358f3788173ffff4117859f42cca8a8dc
Ivan Tlaseca <ivantlaseca@hotmail.com>
2025-02-17 23:37:08 -0500

    Fix Contact email

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
         <div className="contact-container">
             <h2>Get In Touch</h2>
             <p>Any questions, comments, or concerns, feel free to reach me at my email.</p>
-            <a href="mailto:ivantlasecaes@gmail.com" className='cta-btn'>Contact Me</a>
+            <a href="mailto:ivantlaseca@itdevcoach.com" className='cta-btn'>Contact Me</a>
         </div>
     )
 }
